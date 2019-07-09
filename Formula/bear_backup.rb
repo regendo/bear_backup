e0356cc86ac5fe11d313417be71def86c2817aec
Ronan Chilvers <ronan@thelittledot.com>
2019-07-09 21:15:16 +0100

    Trying to fix formula

--- Formula/bear_backup.rb
+++ Formula/bear_backup.rb
@@ -13,7 +13,7 @@ class BearBackup < Formula
   plist_options :manual => "bear_backup"
 
   def plist
-    <<-EOF.undent
+    <<-EOF
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
         "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
